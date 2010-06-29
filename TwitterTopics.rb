02348032a2cb6dfb955fd8efdfb535ada33ea495
jgulla <jgulla@constantcontact.com>
2010-06-29 11:26:57 -0400

    Add code to work offline

--- TwitterTopics.rb
+++ TwitterTopics.rb
@@ -7,8 +7,32 @@ class TwitterTopics
   def get_topics(woeid)
     c = HTTPClient.new
 
-    content =  c.get_content("http://api.twitter.com/1/trends/#{woeid}.xml")
+    content = %{
+      <?xml version="1.0" encoding="UTF-8"?>
+      <matching_trends type="array">
+      <trends created_at="2010-06-29T15:08:13Z" as_of="2010-06-29T15:22:10Z">
+        <locations>
+          <location>
+            <woeid>44418</woeid>
+            <name>London</name>
+          </location>
+        </locations>
+        <trend query="Harry+Potter" url="http://search.twitter.com/search?q=Harry+Potter">Scary Potter</trend>
+        <trend query="Over+Capacity" url="http://search.twitter.com/search?q=Over+Capacity">Over Capacity</trend>
+        <trend query="%23worldcup" url="http://search.twitter.com/search?q=%23worldcup">#worldcup</trend>
+        <trend query="Japan" url="http://search.twitter.com/search?q=Japan">Japan</trend>
+        <trend query="%23oneletterextrabands" url="http://search.twitter.com/search?q=%23oneletterextrabands">#oneletterextrabands</trend>
+        <trend query="%23jpn" url="http://search.twitter.com/search?q=%23jpn">#jpn</trend>
+        <trend query="Paraguay" url="http://search.twitter.com/search?q=Paraguay">Paraguay</trend>
+        <trend query="Spain" url="http://search.twitter.com/search?q=Spain">Spain</trend>
+        <trend query="Honda" url="http://search.twitter.com/search?q=Honda">Honda</trend>
+        <trend query="Echt" url="http://search.twitter.com/search?q=Echt">Echt</trend>
+      </trends>
+      </matching_trends>
+    }
 
+    # comment out following line to use offfline
+    content =  c.get_content("http://api.twitter.com/1/trends/#{woeid}.xml")
     doc = Nokogiri::HTML(content)
     topic_names = Array.new
     
--- app.rb
+++ app.rb
@@ -9,12 +9,11 @@ get "/" do
   redirect "index.html"
 end
 
-
-
 get "/topics" do
   content_type "application/json"
   tc = TwitterTopics.new
   topics = tc.get_topics(params[:woeid])
+
   pl = params[:place]
   puts params[:woeid]
   puts params[:place]
@@ -22,12 +21,13 @@ get "/topics" do
   erb :topics, :layout => false, :locals => {:loc=>topics,:place=>pl}
 end
 
-
-
 get "/locations.json" do
   content_type "application/json"
   c = HTTPClient.new
   c.get_content("http://api.twitter.com/1/trends/available.json")
+  
+  # uncomment next line if offline
+  #  %{ [{"placeType":{"code":12,"name":"Country"},"countryCode":"IE","url":"http://where.yahooapis.com/v1/place/23424803","name":"Fryerland","woeid":23424803,"country":"Ireland"},{"country":"Mexico","placeType":{"code":12,"name":"Country"},"countryCode":"MX","name":"Mexico","url":"http://where.yahooapis.com/v1/place/23424900","woeid":23424900},{"country":"United Kingdom","placeType":{"code":12,"name":"Country"},"name":"United Kingdom","url":"http://where.yahooapis.com/v1/place/23424975","countryCode":"GB","woeid":23424975},{"countryCode":"US","woeid":2514815,"url":"http://where.yahooapis.com/v1/place/2514815","name":"Washington","placeType":{"code":7,"name":"Town"},"country":"United States"},{"placeType":{"code":7,"name":"Town"},"countryCode":"US","url":"http://where.yahooapis.com/v1/place/2367105","name":"Boston","woeid":2367105,"country":"United States"},{"country":"Brazil","placeType":{"code":7,"name":"Town"},"countryCode":"BR","name":"Sao Paulo","url":"http://where.yahooapis.com/v1/place/455827","woeid":455827},{"country":"United States","placeType":{"code":7,"name":"Town"},"countryCode":"US","name":"Baltimore","url":"http://where.yahooapis.com/v1/place/2358820","woeid":2358820},{"country":"","placeType":{"code":19,"name":"Supername"},"countryCode":null,"name":"Worldwide","url":"http://where.yahooapis.com/v1/place/1","woeid":1},{"country":"United States","placeType":{"code":7,"name":"Town"},"countryCode":"US","name":"New York","url":"http://where.yahooapis.com/v1/place/2459115","woeid":2459115},{"countryCode":"US","country":"United States","name":"San Antonio","url":"http://where.yahooapis.com/v1/place/2487796","placeType":{"code":7,"name":"Town"},"woeid":2487796},{"country":"United States","placeType":{"code":12,"name":"Country"},"name":"United States","url":"http://where.yahooapis.com/v1/place/23424977","countryCode":"US","woeid":23424977},{"country":"Brazil","placeType":{"code":12,"name":"Country"},"name":"Brazil","url":"http://where.yahooapis.com/v1/place/23424768","countryCode":"BR","woeid":23424768},{"country":"United States","placeType":{"code":7,"name":"Town"},"countryCode":"US","name":"Philadelphia","url":"http://where.yahooapis.com/v1/place/2471217","woeid":2471217},{"placeType":{"code":7,"name":"Town"},"woeid":2379574,"countryCode":"US","url":"http://where.yahooapis.com/v1/place/2379574","country":"United States","name":"Chicago"},{"countryCode":"US","country":"United States","name":"Houston","url":"http://where.yahooapis.com/v1/place/2424766","placeType":{"code":7,"name":"Town"},"woeid":2424766},{"country":"United States","placeType":{"code":7,"name":"Town"},"name":"Los Angeles","url":"http://where.yahooapis.com/v1/place/2442047","countryCode":"US","woeid":2442047},{"countryCode":"US","country":"United States","name":"San Francisco","url":"http://where.yahooapis.com/v1/place/2487956","placeType":{"code":7,"name":"Town"},"woeid":2487956},{"country":"Canada","placeType":{"code":12,"name":"Country"},"name":"Canada","url":"http://where.yahooapis.com/v1/place/23424775","countryCode":"CA","woeid":23424775},{"country":"United States","placeType":{"code":7,"name":"Town"},"countryCode":"US","name":"Atlanta","url":"http://where.yahooapis.com/v1/place/2357024","woeid":2357024},{"countryCode":"US","country":"United States","name":"Fort Worth","url":"http://where.yahooapis.com/v1/place/2406080","placeType":{"code":7,"name":"Town"},"woeid":2406080},{"placeType":{"code":7,"name":"Town"},"woeid":2388929,"countryCode":"US","url":"http://where.yahooapis.com/v1/place/2388929","country":"United States","name":"Dallas"},{"country":"United States","placeType":{"code":7,"name":"Town"},"countryCode":"US","name":"Seattle","url":"http://where.yahooapis.com/v1/place/2490383","woeid":2490383},{"country":"United Kingdom","placeType":{"code":7,"name":"Town"},"countryCode":"GB","name":"London","url":"http://where.yahooapis.com/v1/place/44418","woeid":44418}]}
 end
 
 __END__
